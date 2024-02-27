8f48f90b59f7602e95ffcfd51ab9ec265bbf1487
omarhossam110 <m.omar.hossam@gmail.com>
2024-02-27 06:02:29 +0200

    Add real API service

--- src/components/App.js
+++ src/components/App.js
@@ -71,7 +71,7 @@ function App() {
   const maxPoints = questions.reduce((prev, cur) => prev + cur.points, 0);
 
   useEffect(function () {
-    fetch("http://localhost:8000/questions")
+    fetch("https://the-react-quiz-server.onrender.com/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
